0303b5732b5a533270ca005a1c471a435b01ee61
Krittithee Tuncharoen <maikittitee@Krittithees-MacBook-Pro.local>
2022-07-22 21:07:11 +0700

    c02 done

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -15,18 +15,18 @@ void	ft_putstr_non_printable(char *str)
 		if ((str[i] >= 0 && str[i] <= 31) || (str[i] >= 127)) //0-31 127+
 		{
 			ft_putchar('\\');
-			ft_putchar("0123456789ABCDEF"[str[i] / 16]);
-			ft_putchar("0123456789ABCDEF"[str[i] % 16]);
+			ft_putchar("0123456789abcdef"[str[i] / 16]);
+			ft_putchar("0123456789abcdef"[str[i] % 16]);
 		}
 		else
 			ft_putchar(str[i]);
 		i++;
 	} 
 }
-
+/*
 int main()
 {
-	char str[40]= "Coucou\ntu vas bien ?";
+	char str[40]= "\012";
 	ft_putstr_non_printable(str);
 	return (0);
-}
+}*/
