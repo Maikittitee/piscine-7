5d3e0f202ba61706dd8a4adb7522bb6065370d1b
Krittithee Tuncharoen <maikittitee@Krittithees-MacBook-Pro.local>
2022-07-22 16:42:45 +0700

    c02 ex10 done

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+unsigned int ft_strlcpy(char *dest, char *src,unsigned int size)
+{
+	int src_size;
+
+	src_size = 0;
+	while (src[src_size] != '\0')
+	{
+		src_size++;
+	}
+	return(src_size);
+}
+/*
+int main(void)
+{
+	char src[20] = "Hello";
+	char dest[10];
+	printf("%d", ft_strlcpy(dest, src, 10));
+}*/
