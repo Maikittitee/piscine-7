f3836fa676901c27f9b1ea2b51471cd954bd024f
Krittithee Tuncharoen <maikittitee@Krittithees-MacBook-Pro.local>
2022-08-04 12:28:33 +0700

    c07 by alias

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -2,13 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int ft_strlen(char *str);
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
 
 int	ft_strstrlen(int size, char **str)
 {
 	int	i;
-	int j;
-	int sum;
+	int	sum;
 
 	i = 0;
 	sum = 0;
@@ -42,56 +49,47 @@ char	*ft_strcat(char *dest, char *src)
 	return (dest);
 }
 
-int ft_strlen(char *str)
-{
-    int i;
 
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	int	i;
+	char	*newstr;
+
+	
 	i = 0;
-	while (str[i])
-		i++;
-    return (i);
-}
-
-char *ft_strjoin(int size, char **strs, char *sep)
-{
-    int i;
-    int j;
-    char *newstr;
-
-
-    newstr = malloc(ft_strstrlen(size,strs) + size);
-    if (!newstr)
-        return (0);
-    while (i < size)
-    {
-        ft_strcat(newstr,strs[i]);
+	newstr = malloc((ft_strstrlen(size,strs) + size) * sizeof (char));
+	if (!newstr)
+		return (0);
+	while (i < size)
+	{
+		ft_strcat(newstr,strs[i]);
 		if (i != size - 1)
 			ft_strcat(newstr,sep);
 		i++;
 	}
 	return (newstr);
 }
-/*
-int	main(void)
-{
-	int		index;
-	char	**strs;
-	char	*separator;
-	char	*result;
-	int	size;
 
-	size = 3;
-	strs = (char **)malloc(3 * sizeof(char *));
-	strs[0] = (char *)malloc(sizeof(char) * 5 + 1);
-	strs[1] = (char *)malloc(sizeof(char) * 7 + 1);
-	strs[2] = (char *)malloc(sizeof(char) * 14 + 1);
-	strs[0] = "Hello";
-	strs[1] = "friend ";
-	strs[2] = "you are awesome";
-	separator = ",";
-	result = ft_strjoin(size, strs, separator);
-	//printf("%d\n", ft_strstrlen(size,strs));
-	printf("%s\n",result);
-	free(result);
-}
-*/
+/*int main(int argc, char **argv)
+{
+	char *strs[10];
+	char *str;
+	int	i;
+
+	strs[0] = "abc";
+	strs[1] = "def";
+	strs[2] = "";
+	strs[3] = "m";
+	strs[4] = "badfsfsaf";
+
+	i = 3;
+	if (argc>1)
+	{
+		i = argv[1][0] - '0';
+	}
+
+	str = ft_strjoin(i,strs, " x ");
+	printf("%s\n",str);
+	free(str);
+	return 0;
+}*/
